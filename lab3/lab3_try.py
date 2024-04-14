f3632639633c5353c14bccc59992b5e6f18232a6
Giulio Titonel <giuliorossi11@yahoo.it>
2024-04-14 18:17:18 +0200

    calculate within matrix. Between matrix started but wrong

--- lab3/lab3_try.py
+++ lab3/lab3_try.py
@@ -43,8 +43,7 @@ def plot(arrXtoPlot, arrYtoPlot, color, label):
         plt.legend()
 
 
-def loadMatrixTemp2():
-    dataSetMatrix, completeDataSetMatrix = load('..\lab2\iris.csv')
+def doPCA(dataSetMatrix, completeDataSetMatrix):
     covMatrix = getCovarianceMatrix(dataSetMatrix)
     print('cov matrix:')
     print(covMatrix)
@@ -75,11 +74,60 @@ def loadMatrixTemp2():
     plot(np.matrix(virginicaMatrix)[:, 0], np.matrix(virginicaMatrix)[:, 1],'green', 'virginica')
     plt.show()
 
+#pass transposed matrix!
+def calculateWithinCovarianceMatrix(matrixSetosaT, matrixVersicolorT, matrixVirginicaT):
+    # betweenClassCovMatrix
+    #first compute average for each class:
+    avgSetosa = np.mean(matrixSetosaT, axis=1)
+    avgVersicolor = np.mean(matrixVersicolorT, axis=1)
+    avgVirginica = np.mean(matrixVirginicaT, axis=1)
+
+    #calculate the within diff matrices:
+    matrixDiffSetosa = matrixSetosaT - avgSetosa.reshape(avgSetosa.size, 1)
+    matrixDiffVersicolor = matrixVersicolorT - avgVersicolor.reshape(avgVersicolor.size, 1)
+    matrixDiffVirginica = matrixVirginicaT - avgVirginica.reshape(avgVirginica.size, 1)
+    
+    #calculate the within components of cov matrix one by one:
+    matrixPartSetosa = matrixDiffSetosa @ matrixDiffSetosa.T
+    matrixPartVersicolor = matrixDiffVersicolor @ matrixDiffVersicolor.T
+    matrixPartVirginica = matrixDiffVirginica @ matrixDiffVirginica.T
+
+    withinCovMatrix = (matrixPartSetosa+matrixPartVersicolor+matrixPartVirginica) / (150)
+    return withinCovMatrix
+
+
+
+def calculateBetweenCovarianceMatrix(matrixSetosaT, matrixVersicolorT, matrixVirginicaT, matrixGlobalT):
+    #calculate between cov matrix:
+    avgGlobal = np.mean(matrixGlobalT, axis=1)
+    
+    #calculate diff matrices
+    matrixDiffSetosa = matrixSetosaT - avgGlobal.reshape(avgGlobal.size, 1)
+    matrixDiffVersicolor = matrixVersicolorT - avgGlobal.reshape(avgGlobal.size, 1)
+    matrixDiffVirginica = matrixVirginicaT - avgGlobal.reshape(avgGlobal.size, 1)
+    
+    #calculate the between components of cov matrix one by one:
+    matrixPartSetosa =  matrixDiffSetosa @ matrixDiffSetosa.T
+    matrixPartVersicolor = matrixDiffVersicolor @ matrixDiffVersicolor.T
+    matrixPartVirginica = matrixDiffVirginica @ matrixDiffVirginica.T
+    #sum and make average:
+    betweenMatrixTotal = (matrixPartSetosa*matrixPartSetosa.shape[0] + matrixPartVersicolor*matrixPartVersicolor.shape[0] + matrixPartVirginica*matrixPartVirginica.shape[0])
+    betweenCovMatrix = betweenMatrixTotal / (matrixPartSetosa.shape[0] + matrixPartVersicolor.shape[0] + matrixPartVirginica.shape[0])
+    return betweenCovMatrix
 
 if __name__ == '__main__':
-    # loadMatrixTemp2()
+    dataSetMatrix, completeDataSetMatrix = load('..\lab2\iris.csv')
+    # doPCA(dataSetMatrix, completeDataSetMatrix)
+
     #issue: graph result is not the same as the wanted one. 
     #Anyway, covariance matrix is fine
 
-
-
+    matrixSetosa, matrixVersicolor, matrixVirginica = divideMatrix(completeDataSetMatrix)
+    #debug:
+    print(np.array(matrixSetosa).T)
+    withinCovMatrix = calculateWithinCovarianceMatrix(np.array(matrixSetosa).astype(float).T, np.array(matrixVersicolor).astype(float).T, np.array(matrixVirginica).astype(float).T)
+    print('within cov matrix:')
+    print(withinCovMatrix)
+    betweenCovMatrix = calculateBetweenCovarianceMatrix(np.array(matrixSetosa).astype(float).T, np.array(matrixVersicolor).astype(float).T, np.array(matrixVirginica).astype(float).T, np.array(dataSetMatrix).astype(float).T)
+    print('between cov matrix:')
+    print(betweenCovMatrix)
